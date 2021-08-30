b516d5df86ed452a4ea1ce4982654c6d69fe6ee7
Adarsh shakya <adarsh.shakya_cs20@gla.ac.in>
2021-08-30 17:04:20 +0530

    sum

---
+++ src/Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a=3;
+        int b=5;
+        int s;
+        s=a+b;
+        System.out.print("sum = " +s);
+    }
+}
